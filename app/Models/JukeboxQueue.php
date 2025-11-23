ad9fb30e47d4223f5e9c0efe7845c29b5d22ff98
Max Hertel <maxherteljr@gmail.com>
2025-11-23 20:57:25 -0300

    dash3

--- app/Models/JukeboxQueue.php
+++ app/Models/JukeboxQueue.php
@@ -1,14 +1,20 @@
 <?php
 
-namespace App\Models;
+use App\Models\User;
 
-use Illuminate\Database\Eloquent\Model;
 class JukeboxQueue extends Model
 {
     protected $table = 'jukebox_queue';
+
     protected $fillable = [
         'user_id',
         'track_name',
-        'track_uri'
+        'track_uri',
+        'is_playing'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
