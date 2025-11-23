2c4928b3ee03ee53745487be7c88c8b1036367c5
Max Hertel <maxherteljr@gmail.com>
2025-11-23 20:27:47 -0300

    build5

--- config/services.php
+++ config/services.php
@@ -34,5 +34,10 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'spotify' => [
+    'client_id' => env('SPOTIFY_CLIENT_ID'),
+    'client_secret' => env('SPOTIFY_CLIENT_SECRET'),
+    'redirect' => env('SPOTIFY_REDIRECT_URI')
+],
 
 ];
